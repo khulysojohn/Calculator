25b10e0b47b3d742ad3369ecc2fc326be6d825cc
khulyso <khulyso.dev@gmail.com>
2024-06-18 18:59:52 +0200

    updated files

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const display = document.getElementById("display");
     const buttons = document.querySelectorAll("#buttons button");
+    const darkBtn = document.querySelector(".dark");
+    const lightBtn = document.querySelector(".light");
+
+    darkBtn.addEventListener("click", () =>{
+        document.documentElement.classList.remove("light");
+        document.documentElement.classList.add("dark");
+    });
+
+    lightBtn.addEventListener("click" , () =>{
+document.documentElement.classList.remove("dark");
+document.documentElement.classList.add("light");
+    });
 
     buttons.forEach(button => {
         button.addEventListener("click", () => {
@@ -12,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 display.value = display.value.slice(0, -1);
             } else if (buttonText === "=") {
                 try {
-                    display.value = eval(display.value.replace(/x/g, "*").replace(/÷/g, "/"));
+                    display.value = eval(display.value)//.replace(/x/g, "*").replace(/÷/g, "/"));
                 } catch (e) {
                     display.value = "Error";
                 }
